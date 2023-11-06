a58dd4691fb322341a5ec3c9adf717a943e67298
Nikoly18 <61807368+Nikoly18@users.noreply.github.com>
2023-11-06 09:23:13 -0300

    Delete projeto-interdisciplinar-main directory

--- projeto-interdisciplinar-main/assets/js/index.js
+++
@@ -1,9 +0,0 @@
-import { validador } from './validacoes/validador.js';
-
-
-const camposDoFormulario = document.querySelectorAll('[data-validado]');
-
-camposDoFormulario.forEach(campo => {
-  campo.addEventListener('keyup', () => validador(campo));
-  campo.addEventListener('invalid', evento => evento.preventDefault());
-});
--- projeto-interdisciplinar-main/assets/js/radio-button/radio-button.js
+++
@@ -1,9 +0,0 @@
-const inputsRadioCriado = document.querySelectorAll('[data-radio-criado]');
-
-inputsRadioCriado.forEach(input => {
-  input.addEventListener('click', () => {
-    const inputClicado = input.querySelector('input');
-
-    inputClicado.checked = true;
-  });
-});
